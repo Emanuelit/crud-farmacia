e8f5a0e5e90f5cb6fd5988421d6018f94ff970ef
Emanuelit <emanuelitavelino@gmail.com>
2022-03-31 16:46:34 -0300

    Atualizando CRUD

---
+++ src/test/java/org/generation/brazil/farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.brazil.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
